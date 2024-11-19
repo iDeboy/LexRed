02bf66c236753749510d5788485a7cd9e7c3cf2f
iDeboy <Theboygamer77@gmail.com>
2024-11-18 22:48:24 -0600

    A little regex tester made

---
+++ LexRed.Test/Program.cs
@@ -0,0 +1,20 @@
+﻿using LexRed.Brzozowski;
+
+// TODO: Make real tests with a specielized library
+
+CharClass a = CharClass.CreatePos("a");
+CharClass b = CharClass.CreatePos("b");
+CharClass c = CharClass.CreatePos("c");
+
+CharClassRegex aRe = a;
+CharClassRegex bRe = b;
+CharClassRegex cRe = c;
+
+var ab = BrzozowskiRegex.MakeConcat(aRe, bRe);
+var ac = BrzozowskiRegex.MakeConcat(aRe, cRe);
+
+var a_or_b = BrzozowskiRegex.MakeOr(aRe, aRe, bRe);
+
+var or = BrzozowskiRegex.MakeOr(ab, ac, BrzozowskiRegex.EmptyString, a_or_b);
+
+return 0;
