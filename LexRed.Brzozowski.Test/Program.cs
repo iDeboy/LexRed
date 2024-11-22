e5c642d68cec2fd0a8f9e07fa420e1759ce16eb9
iDeboy <Theboygamer77@gmail.com>
2024-11-21 23:41:03 -0600

    Added some tests

--- LexRed.Brzozowski.Test/Program.cs
+++ LexRed.Brzozowski.Test/Program.cs
@@ -34,17 +34,17 @@ var a_or_b_k = BrzozowskiRegex.MakeKleene(a_or_b);
 var re = BrzozowskiRegex.MakeConcat(a_or_b_k, a, a_or_b, a_or_b, a_or_b, a_or_b);
 
 var sw = Stopwatch.StartNew();
-
 var dfa = re.MakeDFA();
-
 Use(dfa);
-Use(dfa.IsMatch("aaaba"));
-
 sw.Stop();
+Console.WriteLine($"Make DFA: {sw.ElapsedMilliseconds}ms");
 
-Console.WriteLine($"Elapsed: {sw.ElapsedMilliseconds}ms");
+sw.Restart();
+Use(dfa.IsMatch("abbbb"));
+sw.Stop();
+Console.WriteLine($"IsMatch DFA: {sw.ElapsedMilliseconds}ms");
 
 return 0;
 
 [MethodImpl(MethodImplOptions.NoInlining)]
-static void Use<T>(T value) => Console.WriteLine(value);
+static void Use<T>(T value) { }
--- LexRed.Thompson.Test/Program.cs
+++ LexRed.Thompson.Test/Program.cs
@@ -1,25 +1,57 @@
-﻿
-using LexRed.Common;
+﻿using LexRed.Common;
 using LexRed.Thompson;
-using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+//CharClassRegex a = CharClass.CreatePos("a");
+//CharClassRegex b = CharClass.CreatePos("b");
+
+//// ab
+//var concat = ThompsonRegex.MakeConcat(a, b);
+
+//// a|ab
+//var union = ThompsonRegex.MakeOr(a, concat, a);
+
+//// (a|ab)*
+//var kleene = ThompsonRegex.MakeKleene(union);
+
+//var nfa = kleene.MakeNFA();
+//var dfa = nfa.ToDFA();
+
+//var r = nfa.IsMatch("aba");
 
 CharClassRegex a = CharClass.CreatePos("a");
 CharClassRegex b = CharClass.CreatePos("b");
 
-// ab
-var concat = ThompsonRegex.MakeConcat(a, b);
+var a_or_b = ThompsonRegex.MakeOr(a, b);
 
-// a|ab
-var union = ThompsonRegex.MakeOr(a, concat, a);
+var a_or_b_k = ThompsonRegex.MakeKleene(a_or_b);
 
-// (a|ab)*
-var kleene = ThompsonRegex.MakeKleene(union);
+// (a|b)*a(a|b)(a|b)(a|b)(a|b)
+var re = ThompsonRegex.MakeConcat(a_or_b_k, a, a_or_b, a_or_b, a_or_b, a_or_b);
 
-var nfa = kleene.MakeNFA();
+var sw = Stopwatch.StartNew();
+var nfa = re.MakeNFA();
+sw.Stop();
+Console.WriteLine($"Make NFA: {sw.ElapsedMilliseconds}ms");
+
+sw.Restart();
 var dfa = nfa.ToDFA();
+Use(dfa);
+sw.Stop();
+Console.WriteLine($"Make DFA: {sw.ElapsedMilliseconds}ms");
 
-var r = nfa.IsMatch("aba");
+sw.Restart();
+Use(nfa.IsMatch("abbbbaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
+sw.Stop();
+Console.WriteLine($"IsMatch NFA: {sw.ElapsedMilliseconds}ms");
 
-
+sw.Restart();
+Use(dfa.IsMatch("abbbbaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
+sw.Stop();
+Console.WriteLine($"IsMatch DFA: {sw.ElapsedMilliseconds}ms");
 
 return 0;
+
+[MethodImpl(MethodImplOptions.NoInlining)]
+static void Use<T>(T value) { }
